daf8326dcc0f510dc096aaf754199c483237664b
Eunjae Lee <hey@eunjae.dev>
2022-06-02 21:24:51 +0200

    use js for layout

--- src/layouts/Blog.svelte
+++ src/layouts/Blog.svelte
@@ -1,6 +1,6 @@
-<script lang="ts">
-    export let title: string;
-    export let lang: 'en' | 'ko';
+<script>
+	export let title;
+	export let lang;
 </script>
 
 <h1 class="text-3xl my-8">{title}</h1>
