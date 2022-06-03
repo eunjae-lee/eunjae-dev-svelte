81174a1a104993eddc3641f28680cc2175cacde4
Eunjae Lee <karis612@gmail.com>
2022-06-03 17:59:51 +0200

    add support for themes

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	import EngKor from './EngKor.svelte';
 	import KorEng from './KorEng.svelte';
 
 	export let currentLang: 'en' | 'ko';
+
+	type Theme = 'light' | 'dark' | 'system';
 
 	let isEnglish = currentLang === 'en';
 	let home: string = isEnglish ? 'Home' : '처음으로';
@@ -10,12 +14,30 @@
 		// { label: 'Item1', href: '/' },
 		{ label: isEnglish ? 'Uses' : '사용하는 것들', href: '/uses' },
 	];
+	let currentTheme: Theme;
+
+	onMount(() => {
+		currentTheme = (document.querySelector('html')!.getAttribute('data-theme') ||
+			'system') as Theme;
+	});
+
+	function setTheme(theme: 'light' | 'dark' | 'system') {
+		const html = document.querySelector('html')!;
+		if (theme === 'system') {
+			html.removeAttribute('data-theme');
+		} else {
+			html.setAttribute('data-theme', theme);
+		}
+		localStorage.setItem('theme', theme);
+		currentTheme = theme;
+	}
 </script>
 
 <div class="navbar">
 	<div class="navbar-start">
 		<div class="dropdown">
-			<button tabindex="0" class="btn btn-ghost md:hidden"
+			<!-- svelte-ignore a11y-label-has-associated-control -->
+			<label tabindex="0" class="btn btn-ghost md:hidden"
 				><svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-6 w-6"
@@ -25,27 +47,64 @@
 					stroke-width="2"
 				>
 					<path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-				</svg></button
+				</svg></label
 			>
 			<ul
 				tabindex="0"
 				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-200 rounded-box w-48"
 			>
-				<li><a href="/">{home}</a></li>
+				<li><a class="opacity-25 hover:opacity-75" href="/">{home}</a></li>
 				{#each menuItems as item (item.label)}
-					<li><a href={item.href}>{item.label}</a></li>
+					<li><a class="opacity-25 hover:opacity-75" href={item.href}>{item.label}</a></li>
 				{/each}
 				<li tabindex="0">
-					<button class="justify-between" title="Choose language">
-						{#if currentLang === 'ko'}
-							<EngKor />
-						{:else}
-							<KorEng />
-						{/if}
+					<button class="opacity-25 hover:opacity-75 justify-between" title="Choose theme">
+						Theme
 					</button>
 					<ul class="p-2 bg-base-200">
-						<li><a href="/en">English</a></li>
-						<li><a href="/ko">Korean</a></li>
+						<li>
+							<button
+								type="button"
+								class={`${currentTheme === 'light' ? 'opacity-75' : 'opacity-25'} hover:opacity-75`}
+								href="/en"
+								on:click={() => setTheme('light')}>Light</button
+							>
+						</li>
+						<li>
+							<button
+								type="button"
+								class={`${currentTheme === 'dark' ? 'opacity-75' : 'opacity-25'} hover:opacity-75`}
+								href="/ko"
+								on:click={() => setTheme('dark')}>Dark</button
+							>
+						</li>
+						<li>
+							<button
+								type="button"
+								class={`${
+									currentTheme === 'system' ? 'opacity-75' : 'opacity-25'
+								} hover:opacity-75`}
+								href="/ko"
+								on:click={() => setTheme('system')}>System</button
+							>
+						</li>
+					</ul>
+				</li>
+				<li tabindex="0">
+					<button class="opacity-25 hover:opacity-75 justify-between" title="Choose language">
+						Language
+					</button>
+					<ul class="p-2 bg-base-200">
+						<li>
+							<a class={`${isEnglish ? 'opacity-75' : 'opacity-25'} hover:opacity-75`} href="/en"
+								>English</a
+							>
+						</li>
+						<li>
+							<a class={`${isEnglish ? 'opacity-25' : 'opacity-75'} hover:opacity-75`} href="/ko"
+								>Korean</a
+							>
+						</li>
 					</ul>
 				</li>
 			</ul>
@@ -59,29 +118,84 @@
 	<div class="navbar-end hidden md:flex">
 		<ul class="menu menu-horizontal p-0">
 			{#each menuItems as item (item.label)}
-				<li><a href={item.href}>{item.label}</a></li>
+				<li><a class="opacity-25 hover:opacity-75" href={item.href}>{item.label}</a></li>
 			{/each}
 
 			<li class="dropdown dropdown-end">
-				<button title="Choose language" tabindex="0" class="btn m-1 btn-ghost "
-					>{#if currentLang === 'ko'}
-						<EngKor />
-					{:else}
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label
+					title="Choose theme"
+					tabindex="0"
+					class="opacity-25 hover:opacity-75 btn m-1 btn-ghost"
+				>
+					<svg viewBox="0 0 24 24" class="w-6 h-6 fill-current"
+						><path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z"
+							class="fill-current"
+						/><path
+							d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z"
+							class="fill-current"
+						/><path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z"
+							class="fill-current"
+						/></svg
+					>
+				</label>
+				<ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-28">
+					<li>
+						<button
+							type="button"
+							class={`${currentTheme === 'light' ? 'opacity-75' : 'opacity-25'} hover:opacity-75`}
+							on:click={() => setTheme('light')}>Light</button
+						>
+					</li>
+					<li>
+						<button
+							type="button"
+							class={`${currentTheme === 'dark' ? 'opacity-75' : 'opacity-25'} hover:opacity-75`}
+							on:click={() => setTheme('dark')}>Dark</button
+						>
+					</li>
+					<li>
+						<button
+							type="button"
+							class={`${currentTheme === 'system' ? 'opacity-75' : 'opacity-25'} hover:opacity-75`}
+							on:click={() => setTheme('system')}>System</button
+						>
+					</li>
+				</ul>
+			</li>
+			<li class="dropdown dropdown-end">
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label
+					title="Choose language"
+					tabindex="0"
+					class="opacity-25 hover:opacity-75 btn m-1 btn-ghost"
+					>{#if isEnglish}
 						<KorEng />
-					{/if}</button
+					{:else}
+						<EngKor />
+					{/if}</label
 				>
 				<ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-28">
-					<li><a class="justify-center" href="/en">English</a></li>
-					<li><a class="justify-center" href="/ko">Korean</a></li>
+					<li>
+						<a
+							class={`${isEnglish ? 'opacity-75' : 'opacity-25'} hover:opacity-75 justify-center`}
+							href="/en">English</a
+						>
+					</li>
+					<li>
+						<a
+							class={`${isEnglish ? 'opacity-25' : 'opacity-75'} hover:opacity-75 justify-center`}
+							href="/ko">Korean</a
+						>
+					</li>
 				</ul>
 			</li>
 		</ul>
 	</div>
 </div>
-
-<style>
-	ul.menu li a,
-	ul.menu li button {
-		@apply opacity-25 hover:opacity-75;
-	}
-</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,15 @@
 <script>
+	import { onMount } from 'svelte';
+
 	import '../app.css';
+
+	onMount(() => {
+		const theme = localStorage.getItem('theme');
+		if (theme === 'light' || theme === 'dark' || theme === 'system') {
+			// @ts-ignore
+			document.querySelector('html').setAttribute('data-theme', theme);
+		}
+	});
 </script>
 
 <div class="flex flex-col justify-center items-start max-w-3xl mx-auto">
@@ -9,6 +19,12 @@
 <style>
 	:global(:root) {
 		@apply bg-[#f9fafb] dark:bg-[#292929];
+	}
+	:global(:root[data-theme='light']) {
+		@apply bg-[#f9fafb];
+	}
+	:global(:root[data-theme='dark']) {
+		@apply bg-[#292929];
 	}
 
 	:global(.card-body) {
