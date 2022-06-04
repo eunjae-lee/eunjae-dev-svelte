d1e8c066c57d89ace5eb2924ef46513bd473af95
Eunjae Lee <hey@eunjae.dev>
2022-06-04 22:57:33 +0200

    adding route

---
+++ src/routes/post/ko/[...slug].svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+	export async function load({ fetch }) {
+		// console.log('# params.slug', params.slug);
+		// const files = import.meta.glob(`/posts/ko/**/index.svx`);
+		// console.log('# files', JSON.stringify(files));
+		const response = await fetch(`/posts-ko?slug=${params.slug}`, {
+			headers: { accept: 'application/json' },
+		});
+		const posts = (await response.json()).posts;
+		console.log('# posts', posts);
+		return { status: 200 };
+	}
+</script>
+
+<!-- <svelte:component this={component} /> -->
