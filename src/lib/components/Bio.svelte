ed9b761c76f3dc6cbacdf9cb87bac7d9aaca74e7
Eunjae Lee <hey@eunjae.dev>
2022-06-06 14:52:55 +0200

    change profile order

--- src/lib/components/Bio.svelte
+++ src/lib/components/Bio.svelte
@@ -7,16 +7,16 @@
 </script>
 
 <div class="flex flex-col sm:flex-row items-center gap-8 sm:gap-16">
+	<div class="sm:order-last avatar drop-shadow-xl">
+		<div class="w-48">
+			<Image src="profile.png" class="rounded-full" />
+		</div>
+	</div>
 	<div>
 		<h1 class="text-4xl sm:text-[3rem] font-extrabold">{name}</h1>
 		<p class="mt-2 sm:mt-4">{title}</p>
 		<p class="mt-4 opacity-75">
 			{description}
 		</p>
-	</div>
-	<div class="avatar drop-shadow-xl">
-		<div class="w-48">
-			<Image src="profile.png" class="rounded-full" />
-		</div>
 	</div>
 </div>
