87ad8c6400697bcdfe916fb6ea388d7d2b74d969
Eunjae Lee <hey@eunjae.dev>
2022-06-06 14:43:26 +0200

    style error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -11,16 +11,49 @@
 		}
 		return {
 			props: {
-				title: `${status}: ${error!.message}`,
+				status,
 			},
 		};
 	}
 </script>
 
-<script>
-	export let title;
+<script lang="ts">
+	import NavBar from '$lib/components/NavBar.svelte';
+	import { onMount } from 'svelte';
+
+	export let status: number;
+	let isKorean = false;
+
+	onMount(() => {
+		isKorean = (window.navigator.languages || []).some(
+			(language) => language === 'ko' || language.split('-')[0] === 'ko'
+		);
+	});
 </script>
 
-{#if title}
-	<h1>{title}</h1>
-{/if}
+<NavBar lang={isKorean ? 'ko' : 'en'} />
+
+<div class="my-32 flex flex-col gap-8 w-full justify-center items-center">
+	<div class="flex items-center gap-2 text-base-content text-2xl">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="stroke-current flex-shrink-0 h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			><path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+			/></svg
+		>
+		{#if status === 404}
+			<span>Page not found.</span>
+		{:else}
+			<span>Error {status}</span>
+		{/if}
+	</div>
+	<div>
+		<a href="/" class="btn">Go home</a>
+	</div>
+</div>
