1a4ecb77e184be4f411ff84e21c64327981e90d4
Eunjae Lee <karis612@gmail.com>
2022-06-07 21:33:29 +0200

    add meta tags

--- src/routes/post/[series]/index.svelte
+++ src/routes/post/[series]/index.svelte
@@ -9,6 +9,7 @@
 				props: {
 					...props,
 					...SERIES[series],
+					series,
 				},
 			};
 		} else {
@@ -28,7 +29,30 @@
 	export let title: string;
 	export let description: string;
 	export let posts: PostMeta[];
+	export let series: string;
+
+	let url = `https://eunjae.dev/post/${series}`;
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta name="author" content="Eunjae Lee" />
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content={url} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<!-- <meta property="og:image" content={ogImage} /> -->
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={url} />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<!-- <meta name="twitter:image" content={ogImage} /> -->
+</svelte:head>
 
 <NavBar lang="en" />
 
--- src/routes/post/en/index.svelte
+++ src/routes/post/en/index.svelte
@@ -5,7 +5,30 @@
 	import type { PostMeta } from '$lib/types';
 
 	export let posts: PostMeta[];
+
+	let title = 'Posts';
+	let url = `https://eunjae.dev/post/en`;
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+	<!-- <meta name="description" content={description} /> -->
+	<meta name="author" content="Eunjae Lee" />
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content={url} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={title} />
+	<!-- <meta property="og:description" content={description} /> -->
+	<!-- <meta property="og:image" content={ogImage} /> -->
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={url} />
+	<meta name="twitter:title" content={title} />
+	<!-- <meta name="twitter:description" content={description} /> -->
+	<!-- <meta name="twitter:image" content={ogImage} /> -->
+</svelte:head>
 
 <NavBar lang="en" />
 
