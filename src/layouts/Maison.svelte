b765de631cb88541555fbec9469657668758e6ce
Eunjae Lee <hey@eunjae.dev>
2022-06-08 11:22:39 +0200

    fix excerpt

--- src/layouts/Maison.svelte
+++ src/layouts/Maison.svelte
@@ -1,12 +1,8 @@
 <script>
 	import Base from './_Base.svelte';
-	export let title;
-	export let created_at;
-	export let lang;
-	export let path;
 </script>
 
-<Base {title} {created_at} {lang} {path}>
+<Base {...$$props}>
 	<div slot="before-title" class="not-prose mb-2">
 		<div class="text-sm breadcrumbs">
 			<ul>
--- src/layouts/Post.svelte
+++ src/layouts/Post.svelte
@@ -1,9 +1,5 @@
 <script>
 	import Base from './_Base.svelte';
-	export let title;
-	export let created_at;
-	export let lang;
-	export let path;
 </script>
 
-<Base {title} {created_at} {lang} {path}><slot /></Base>
+<Base {...$$props}><slot /></Base>
